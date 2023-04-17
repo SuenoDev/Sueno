b499f2e94b8a23bf468039822955f72837579309
Durmiendo <Durmiendo@git>
2023-04-17 23:33:55 +0700

    r

--- src/org/durmiendo/sueno/extend/SuenoBlock.java
+++ src/org/durmiendo/sueno/extend/SuenoBlock.java
@@ -2,27 +2,27 @@ package org.durmiendo.sueno.extend;
 
 
 import arc.graphics.Color;
+import arc.util.Time;
 import mindustry.gen.Building;
 import mindustry.ui.Bar;
 import mindustry.world.Block;
 import org.durmiendo.sueno.content.SAttributes;
 
+import static java.lang.Math.round;
 
 
-public class SuenoBlock extends Block  {
-
+public class SuenoBlock extends Block {
 
 
     public SuenoBlock(String name) {
         super(name);
+        update = true;
         size = 1;
     }
 
 
-
-
     @Override
-    public void setStats(){
+    public void setStats() {
         super.setStats();
         this.stats.add(SuenoStat.temperature, " from @ to @", this.attributes.get(SAttributes.temperatureMin), this.attributes.get(SAttributes.temperatureMax));
     }
@@ -32,21 +32,20 @@ public class SuenoBlock extends Block  {
     public void setBars() {
         super.setBars();
 
-        addBar("uses", (Temperature entity) -> new Bar(
-                () -> "Temperature",
-                () -> Color.white,
-                () -> (entity.temperature + Math.abs(attributes.get(SAttributes.temperatureMin)))/((attributes.get(SAttributes.temperatureMax) + Math.abs(attributes.get(SAttributes.temperatureMin)))/100)/100)
-        );
+        addBar("temperature", (SuenoBlockBuild entity) -> new Bar(
+                () -> "Temperature " + String.format("%.1f",entity.temperature),
+                () -> Color.cyan,
+                () -> ((entity.temperature - attributes.get(SAttributes.temperatureMin)) / (attributes.get(SAttributes.temperatureMax) - attributes.get(SAttributes.temperatureMin)))
+        ));
     }
 
-    public static class Temperature extends Building {
-        public float temperature = 0f;
+    public class SuenoBlockBuild extends Building {
+
+        public float temperature = -100;
+
+        @Override
+        public void update() {
+            temperature -= 1.3f / 60f * Time.delta;
+        }
     }
 }
-/*addBar("uses",
-(TerraformerBuild entity) -> new Bar(
-() -> Core.bundle.format("bar.uses", maxUses - entity.used),
- () -> Pal.accent,
-  () -> ((float) maxUses - entity.used) / maxUses));
-*/
-//attributes.get(SAttributes.temperature) + Math.abs(attributes.get(SAttributes.temperatureMin)))/((attributes.get(SAttributes.temperatureMax) + Math.abs(attributes.get(SAttributes.temperatureMin)))/100)/100
