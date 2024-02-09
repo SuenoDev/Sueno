058aae8fd7e05cbd48476e137e01ca62aa76e206
Durmiendo <Durmiendo@git>
2024-02-09 23:21:48 +0700

    Sueno will a live!

---
+++ core/src/org/durmiendo/sueno/graphics/g3d/SPlanteRenderer.java
@@ -0,0 +1,11 @@
+package org.durmiendo.sueno.graphics.g3d;
+
+import mindustry.graphics.g3d.PlanetParams;
+import mindustry.graphics.g3d.PlanetRenderer;
+
+public class SPlanteRenderer extends PlanetRenderer {
+
+    public void render(PlanetParams s) {
+        super.render(s);
+    }
+}
---
+++ core/src/org/durmiendo/sueno/satellites/CharSatellite.java
@@ -0,0 +1,27 @@
+package org.durmiendo.sueno.satellites;
+
+import arc.math.Mathf;
+import arc.math.geom.Vec3;
+import arc.util.Time;
+import mindustry.Vars;
+import mindustry.type.Planet;
+import mindustry.ui.Fonts;
+
+public class CharSatellite extends CelestialBody{
+    public char name;
+    public CharSatellite(float r, float pitch, float yaw, Planet planet, char name) {
+        super(r, pitch, yaw, planet);
+
+        this.name = name;
+    }
+
+    @Override
+    public void draw() {
+        angle += speed * Time.delta;
+        angle %= 2 * Mathf.pi;
+
+        position = newPos();
+        Vec3 e = Vars.renderer.planets.cam.project(position);
+        Fonts.def.draw(String.valueOf(name), e.x, e.y);
+    }
+}
