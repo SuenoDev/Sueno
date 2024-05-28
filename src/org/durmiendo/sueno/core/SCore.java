4080c5feb8082121d22ebcb9d9bc5f0e489d3ae7
durmiendo <SergeyZzzPlay3@gmail.com>
2024-05-28 12:19:47 +0700

    [SEND] TEST - A - B

--- src/org/durmiendo/sueno/core/SCore.java
+++ src/org/durmiendo/sueno/core/SCore.java
@@ -2,11 +2,11 @@ package org.durmiendo.sueno.core;
 
 import arc.Core;
 import arc.graphics.g2d.TextureRegion;
+import arc.util.Log;
 import mindustry.Vars;
 import mindustry.mod.Mod;
 import mma.annotations.ModAnnotations;
 import org.durmiendo.sueno.content.SLoader;
-import org.durmiendo.sueno.utils.SLog;
 
 @ModAnnotations.AnnotationSettings(
         rootPackage = "org.durmiendo.sueno",
@@ -22,15 +22,15 @@ public class SCore extends Mod {
     @Override
     public void loadContent(){
         SVars.sueno = Vars.mods.getMod(getClass());
-
+        Log.info("load Sueno content");
         SLoader.load();
     }
 
     @Override
     public void init() {
-        SLog.info("extended logs: " + SVars.extendedLogs);
+        Log.info("extended logs: " + SVars.extendedLogs);
+        Log.info("load Sueno settings");
 
-        SLog.info("start load settings");
         Setter.load();
     }
 
